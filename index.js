d88c9eecb68d611c4c9f2516d0d1d6ed970a29ad
Apple <apple@Apples-MacBook-Pro-75.local>
2019-07-29 14:47:13 -0700

    first upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var countdownDate = new Date("April 8, 2020 08:00:00").getTime();
+
+var x = setInterval(function(){
+  var now = new Date().getTime();
+  var distance = countdownDate - now;
+  var days = Math.floor(distance/(1000*60*60*24));
+  var hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+  var minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+  var seconds = Math.floor((distance % (1000*60))/ 1000);
+  document.getElementById("screentext").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+     clearInterval(x);
+     document.getElementById("screentext").innerHTML = "TIME'S OUT";
+   }
+
+}, 1000);
